60710ab0da889418a983e6ad18df81a3f5cf05f8
reem-baniali <90187792+reem-baniali@users.noreply.github.com>
2021-10-30 10:49:55 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -12,8 +12,8 @@ let err = document.querySelectorAll(".errMessage");
 //declare re1jex for email
 let regexEmail = /^[A-z0-9._-]+@(gmail|yahoo|hotmail).com$/;
 let regexMobile = /^[0][7][7][0-9]{7}$/
-let regexPass = /^[A-z0-9._-*$@]{6,18}$/;
-//validation function
+let regexPass = /^[A-Z]{6,18}$/;
+//validation functZion
 function validation() {
   if (email.value == "") {
     err[0].textContent = "please add email";
